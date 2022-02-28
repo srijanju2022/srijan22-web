9e49b4a846bef219484da22c2c87d3e10ded4edb
kishanhitk <kishansharma1231@gmail.com>
2022-02-28 12:44:49 +0530

    feat: add src folder

---
+++ src/components/componentDummy.tsx
@@ -0,0 +1,7 @@
+export const Dummy = () => {
+  return (
+    <button className="py-2 px-4 m-2 rounded-md bg-pink-600 hover:bg-pink-500 text-white border ">
+      Dummy
+    </button>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Dummy } from "../components/componentDummy";
 
 const Home: NextPage = () => {
   return (
@@ -9,10 +10,11 @@ const Home: NextPage = () => {
         <meta name="description" content="Srijan'22 - Jadavpur University" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex justify-center m-10">
+      <main className="flex flex-col items-center justify-center m-10">
         <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
           Srijan&apos;22
         </h1>
+        <Dummy />
       </main>
     </div>
   );
