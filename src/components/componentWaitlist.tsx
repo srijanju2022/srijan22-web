6c410a0d8fcc531626ff3d8aabfdc9d242d18a4c
kishanhitk <kishansharma1231@gmail.com>
2022-03-03 11:58:58 +0530

    feat: imrpove navbar

--- src/components/componentWaitlist.tsx
+++ src/components/componentWaitlist.tsx
@@ -1,7 +1,5 @@
-import { Footer } from "./componentFooter";
 import { HeroSection } from "./componentHeroSection";
 import { PastSponsor } from "./componentPastSponsor";
-import Emblem from "./logos/emblem";
 
 export const Waitlist = () => {
   return (
