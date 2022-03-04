d99d71418050355f47b868f25cb88a2ecabfdb65
kishanhitk <kishansharma1231@gmail.com>
2022-03-04 20:57:47 +0530

    feat: imrpove footfall section | add responsiveness

--- src/components/componentFootfall.tsx
+++ src/components/componentFootfall.tsx
@@ -1,22 +1,26 @@
 import { FootFallCircles } from "./componentFootfallCircle";
 
 export const FootFall = () => {
-    return(
-        <div className="flex flex-col mb-20 text-white">
-            <div className="flex flex-row mb-20">
-                <h1 className=" font-bold text-6xl mr-20">Our Footfalls:</h1>
-                <p>Social Media is an amazing tool, but it&apos;s really the face-to-face interaction that makes 
-                a long-lasting <br/> impact. The following data is calculated on the basis of number of On-Day registrations, 
-                feedback forms <br/> and ID Cards.</p>
-            </div>
-            <div className="flex flex-row justify-between items-center font-semibold" style={{
-                paddingLeft: "10%",
-                paddingRight: "10%",
-            }}>
-            
-                <FootFallCircles />
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="my-32 text-white">
+      <div className="flex flex-wrap space-y-5 mx-10 flex-row mb-20">
+        <h1 className="font-bold text-6xl mr-20">Our Footfalls:</h1>
+        <p>
+          Social Media is an amazing tool, but it&apos;s really the face-to-face
+          interaction that makes a long-lasting impact. The following data is
+          calculated on the basis of number of On-Day registrations, feedback
+          forms and ID Cards.
+        </p>
+      </div>
+      <div
+        className="flex flex-wrap justify-between items-end space-y-10 font-semibold"
+        style={{
+          paddingLeft: "10%",
+          paddingRight: "10%",
+        }}
+      >
+        <FootFallCircles />
+      </div>
+    </div>
+  );
 };
--- src/components/componentFootfallCircle.tsx
+++ src/components/componentFootfallCircle.tsx
@@ -1,31 +1,62 @@
 export const FootFallCircles = () => {
-    return(
-        <>
-            <div className="w-40 h-40 rounded-full bg-orange-400 text-center border-2" style={{
-                boxShadow: "10px 8px white"
-            }}>
-                <div className="h-full flex justify-center items-center ">15000+</div>
-            </div>
-            <div className="w-32 h-32 rounded-full bg-rose-600 text-center border-2" style={{
-                boxShadow: "10px 8px white"
-            }}>
-                <div className="h-full flex justify-center items-center ">11200</div>
-            </div>
-            <div className="w-28 h-28 rounded-full bg-orange-400 text-center border-2" style={{
-                boxShadow: "10px 8px white"
-            }}>
-                <div className="h-full flex justify-center items-center ">10860</div>
-            </div>
-            <div className="w-24 h-24 rounded-full bg-rose-600 text-center border-2" style={{
-                boxShadow: "10px 8px white"
-            }}>
-                <div className="h-full flex justify-center items-center ">8142</div>
-            </div>
-            <div className="w-20 h-20 rounded-full bg-orange-400 text-center border-2" style={{
-                boxShadow: "10px 8px white"
-            }}>
-                <div className="h-full flex justify-center items-center ">6270</div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-col items-center space-y-10">
+        <div
+          className="w-40 h-40 rounded-full bg-orange-400 text-center border-2"
+          style={{
+            boxShadow: "10px 8px white",
+          }}
+        >
+          <div className="h-full flex justify-center items-center ">15000+</div>
+        </div>
+        <h6>Srijan&apos;20</h6>
+      </div>
+
+      <div className="flex flex-col items-center space-y-10">
+        <div
+          className="w-32 h-32 rounded-full bg-rose-600 text-center border-2"
+          style={{
+            boxShadow: "10px 8px white",
+          }}
+        >
+          <div className="h-full flex justify-center items-center ">11200</div>
+        </div>
+        <h6>Srijan&apos;19</h6>
+      </div>
+      <div className="flex flex-col items-center space-y-10">
+        <div
+          className="w-28 h-28 rounded-full bg-orange-400 text-center border-2"
+          style={{
+            boxShadow: "10px 8px white",
+          }}
+        >
+          <div className="h-full flex justify-center items-center ">10860</div>
+        </div>
+        <h6>Srijan&apos;18</h6>
+      </div>
+      <div className="flex flex-col items-center space-y-10">
+        <div
+          className="w-24 h-24 rounded-full bg-rose-600 text-center border-2"
+          style={{
+            boxShadow: "10px 8px white",
+          }}
+        >
+          <div className="h-full flex justify-center items-center ">8142</div>
+        </div>
+        <h6>Srijan&apos;17</h6>
+      </div>
+      <div className="flex flex-col items-center space-y-10">
+        <div
+          className="w-20 h-20 rounded-full bg-orange-400 text-center border-2"
+          style={{
+            boxShadow: "10px 8px white",
+          }}
+        >
+          <div className="h-full flex justify-center items-center ">6270</div>
+        </div>
+        <h6>Srijan&apos;16</h6>
+      </div>
+    </>
+  );
+};
