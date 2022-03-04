214efc6fb6ecde043a482da330e33f095860614d
kishanhitk <kishansharma1231@gmail.com>
2022-03-04 21:32:29 +0530

    feat: add supabase | connect waitlist form with supabase

--- src/components/componentWaitlistForm.tsx
+++ src/components/componentWaitlistForm.tsx
@@ -1,12 +1,21 @@
-import { FormEvent } from "react";
+import { useState, FormEvent } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 export const WaitlistForm = () => {
-  const handleSubmit = (e: FormEvent) => {
+  const [email, setEmail] = useState<string | undefined>();
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    const { data, error } = await supabase
+      .from("wishlist_submission")
+      .insert([{ email }]);
   };
+
   return (
     <form className="space-x-3 flex" onSubmit={handleSubmit}>
       <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         type="email"
         required
         className="bg-primary-500 w-full border-2 border-white text-white px-2 py-1 ring-0  active:ring-0 hover:ring-0 focus:ring-0 focus:border-secondary-500 "
